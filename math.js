206b59485cb7583adcf5c06ec01a53739762b343
kyzentyven <kyzentyven@gmail.com>
2025-04-24 21:50:40 +0600

    Added the basic concepts of Math in JavaScript.

---
+++ math.js
@@ -0,0 +1,20 @@
+const min = Math.min(35, 15, 22, 4, 7, 11, 9, 66);
+console.log(min);
+
+const max = Math.max(35, 15, 22, 4, 7, 11, 9, 66);
+console.log(max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(5-10));
+
+console.log(Math.round(2.25));
+console.log(Math.round(4.75));
+
+console.log(Math.floor(4.758));
+console.log(Math.ceil(2.001));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+const Random = Math.round(Math.random()*10);
+console.log(Random);
